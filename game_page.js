8e146d59e0ca7909454c9c05f817440933d0e4b8
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2021-08-23 19:51:39 +0530

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -12,7 +12,7 @@ document.getElementById("player1_name").innerHTML = player1_name + ":";
 document.getElementById("player2_name").innerHTML = player2_name + ":";
 
 document.getElementById("player1_score").innerHTML = player1_score;
-document.getElementById("player1_score").innerHTML = player2_score;
+document.getElementById("player2_score").innerHTML = player2_score;
 
 document.getElementById("player_question").innerHTML = player1_name;
 document.getElementById("player_answer").innerHTML = player2_name;
