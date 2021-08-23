66932c9eb30012e6899a9b470d57b320a306e0ed
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2021-08-23 21:14:47 +0530

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -6,7 +6,7 @@ player2_score = 0;
 
 number_1 = document.getElementById("word1").value;
 number_2 = document.getElementById("word2").value;
-number = number_1 * number_2
+number = parseInt(number_1) * parseInt(number_2)
 
 document.getElementById("player1_name").innerHTML = player1_name + ":";
 document.getElementById("player2_name").innerHTML = player2_name + ":";
@@ -24,4 +24,39 @@ function send() {
     row = question_word + input_box + check_button;
     document.getElementById("output").innerHTML = row;
     document.getElementById("word").value = "";
+}
+
+function check() {
+ get_answer = document.getElementById("input_check_box").value;
+answer = number;
+console.log("Answer is" + answer);
+
+if (answer == word) {
+    if (answer_turn == "player1") {
+        player1_score = player1_score + 1;
+        document.getElementById("player1_score").innerHTML=player1_score;
+    }
+    else {
+        player2_score = player2_score + 1;
+        document.getElementById("player2_score").innerHTML=player2_score;
+    }
+}
+if (question_turn == "player1") {
+    question_turn = "player2";
+    document.getElementById("player_question").innerHTML ="Question Turn : "+player2_name ;
+}
+else{
+    question_turn = "player1";
+    document.getElementById("player_question").innerHTML ="Question Turn : "+player1_name ;
+}
+
+if (answer_turn == "player1") {
+    answer_turn = "player2";
+    document.getElementById("player_answer").innerHTML ="Answer Turn : "+player2_name ;
+}
+else{
+    answer_turn = "player1";
+    document.getElementById("player_answer").innerHTML ="Answer Turn : "+player1_name ;
+}
+document.getElementById("output").innerHTML="";
 }
